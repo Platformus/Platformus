2ca752389fea89efea1773e9fee4ca3295c6f460
KANekT <kanektikys@ya.ru>
2016-03-18 16:47:13 +0400

    Add RenderPartialViewToString to BaseController

--- src/Platformus.Barebone/Controllers/ControllerBase.cs
+++ src/Platformus.Barebone/Controllers/ControllerBase.cs
@@ -19,5 +19,31 @@ namespace Platformus.Barebone.Controllers
     {
       return this.Redirect(this.Request.Path.Value + this.Request.QueryString.Value);
     }
+    
+    public string RenderPartialViewToString(string viewName, object model)
+    {
+      var actionContext = ActionContext;
+      if (string.IsNullOrEmpty(viewName))
+        viewName = actionContext.ActionDescriptor.Name;
+
+      ViewData.Model = model;
+
+      using (var sw = new StringWriter())
+      {
+        var engine = Resolver.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
+        if (engine == null)
+          return null;
+
+        var viewResult = engine.FindPartialView(actionContext, viewName);
+
+        var viewContext = new ViewContext(actionContext, viewResult.View, ViewData, TempData, sw, new HtmlHelperOptions());
+
+        var t = viewResult.View.RenderAsync(viewContext);
+        t.Wait();
+
+        return sw.GetStringBuilder().ToString();
+      }
+    }
+
   }
 }
