1f4560171f267e24289ae8b3a14d8703f3d051aa
Dmitry Sikorsky <dmitry@sikorsky.pro>
2017-06-11 09:36:41 +0300

    Fixed #59

--- src/Platformus.Menus.Frontend/ViewModels/Shared/MenuItem/MenuItemViewModel.cs
+++ src/Platformus.Menus.Frontend/ViewModels/Shared/MenuItem/MenuItemViewModel.cs
@@ -11,5 +11,6 @@ namespace Platformus.Menus.Frontend.ViewModels.Shared
     public string Name { get; set; }
     public string Url { get; set; }
     public IEnumerable<MenuItemViewModel> MenuItems { get; set; }
+    public int Level { get; set; }
   }
 }
