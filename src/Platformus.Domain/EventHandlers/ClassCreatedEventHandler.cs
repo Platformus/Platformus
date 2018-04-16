a07e136d44aa9ac02d128648ec66eaf53d3640c8
Dmitry Sikorsky <dmitry@sikorsky.pro>
2018-04-16 23:28:59 +0300

    Fixed #171

---
+++ src/Platformus.Domain/EventHandlers/ClassCreatedEventHandler.cs
@@ -0,0 +1,19 @@
+﻿// Copyright © 2018 Dmitry Sikorsky. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Platformus.Barebone;
+using Platformus.Domain.Data.Entities;
+using Platformus.Domain.Events;
+
+namespace Platformus.Domain.EventHandlers
+{
+  public class ClassCreatedEventHandler : IClassCreatedEventHandler
+  {
+    public int Priority => 1000;
+
+    public void HandleEvent(IRequestHandler requestHandler, Class @object)
+    {
+      requestHandler.GetService<IDomainManager>().InvalidateCache();
+    }
+  }
+}
---
+++ src/Platformus.Domain/EventHandlers/MemberCreatedEventHandler.cs
@@ -0,0 +1,19 @@
+﻿// Copyright © 2018 Dmitry Sikorsky. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Platformus.Barebone;
+using Platformus.Domain.Data.Entities;
+using Platformus.Domain.Events;
+
+namespace Platformus.Domain.EventHandlers
+{
+  public class MemberCreatedEventHandler : IMemberCreatedEventHandler
+  {
+    public int Priority => 1000;
+
+    public void HandleEvent(IRequestHandler requestHandler, Member @object)
+    {
+      requestHandler.GetService<IDomainManager>().InvalidateCache();
+    }
+  }
+}
