b5411477c65ad73516df690786297fe8af37d068
Dmitry Sikorsky <dmitry.a.sikorsky@gmail.com>
2022-04-29 09:47:26 +0300

    Fixed frontend request localization

--- src/Platformus.Core.Frontend/Actions/UseRequestLocalizationAction.cs
+++ src/Platformus.Core.Frontend/Actions/UseRequestLocalizationAction.cs
@@ -43,11 +43,15 @@ namespace Platformus.Core.Frontend.Actions
           cultureManager.GetNotNeutralCulturesAsync().Result.Select(c => new CultureInfo(c.Id)).ToList();
       }
 
+      requestLocalizationOptions.RequestCultureProviders.Remove(
+        requestLocalizationOptions.RequestCultureProviders.OfType<AcceptLanguageHeaderRequestCultureProvider>().First()
+      );
+
       if (serviceProvider.GetService<IConfigurationManager>()["Globalization", "SpecifyCultureInUrl"] == "yes")
         requestLocalizationOptions.RequestCultureProviders.Insert(0, new RouteValueRequestCultureProvider());
 
       applicationBuilder.UseWhen(
-        context => !context.Request.Path.StartsWithSegments(new PathString("/backend")) && !context.Request.Path.StartsWithSegments(new PathString("/img")),
+        context => !context.Request.Path.StartsWithSegments(new PathString("/backend")),
         frontendApplicationBuilder =>
         {
           frontendApplicationBuilder.UseRequestLocalization(requestLocalizationOptions);
--- src/Platformus.Core.Frontend/RouteValueRequestCultureProvider.cs
+++ src/Platformus.Core.Frontend/RouteValueRequestCultureProvider.cs
@@ -2,7 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Linq;
-using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
@@ -13,50 +12,23 @@ namespace Platformus.Core.Frontend
   public class RouteValueRequestCultureProvider : RequestCultureProvider
   {
     public override async Task<ProviderCultureResult> DetermineProviderCultureResult(HttpContext httpContext)
-    {
-      string cultureId = await this.GetDefaultCultureCodeAsync(httpContext);
-      bool specifyCultureInUrl = httpContext.GetConfigurationManager()["Globalization", "SpecifyCultureInUrl"] == "yes";
-
-      if (!specifyCultureInUrl)
-        return new ProviderCultureResult(cultureId);
-      
+    {      
       string url = httpContext.Request.Path;
 
-      if (url == "/")
-        ;
-
-      else if (url.Length < 4)
-        throw new HttpException(HttpStatusCode.NotFound);
-
-      else if (url[0] != '/' || url[3] != '/')
-        throw new HttpException(HttpStatusCode.NotFound);
-
-      else
+      if (url.Length >= 4 && url[0] == '/' && url[3] == '/')
       {
-        cultureId = httpContext.Request.Path.Value.Substring(1, 2);
+        string cultureId = httpContext.Request.Path.Value.Substring(1, 2);
 
-        if (!await this.CheckCultureAsync(httpContext, cultureId))
-          throw new HttpException(HttpStatusCode.NotFound);
+        if (await this.CheckCultureAsync(httpContext, cultureId))
+          return new ProviderCultureResult(cultureId);
       }
 
-      ProviderCultureResult requestCulture = new ProviderCultureResult(cultureId);
-
-      return requestCulture;
-    }
-
-    private async Task<string> GetDefaultCultureCodeAsync(HttpContext httpContext)
-    {
-      Data.Entities.Culture frontendDefaultCulture = await httpContext.GetCultureManager().GetFrontendDefaultCultureAsync();
-
-      return frontendDefaultCulture?.Id ?? DefaultCulture.Id;
+      return null;
     }
 
     private async Task<bool> CheckCultureAsync(HttpContext httpContext, string cultureId)
     {
       ICultureManager cultureManager = httpContext.GetCultureManager();
-
-      if ((await cultureManager.GetNotNeutralCulturesAsync()).Count() == 0)
-        return cultureId == DefaultCulture.Id;
 
       return (await cultureManager.GetNotNeutralCulturesAsync()).Any(c => c.Id == cultureId);
     }
